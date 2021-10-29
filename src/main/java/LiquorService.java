688e00a3abe2a17b0f16685f5dd5197f31a1ac55
chinmaya-trianz <92790019+chinmaya-trianz@users.noreply.github.com>
2021-10-29 19:36:10 +0530

    Update LiquorService.java

--- src/main/java/LiquorService.java
+++ src/main/java/LiquorService.java
@@ -1,30 +1,22 @@
-package com.sample;
-
-import com.sample.model.LiquorType;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class LiquorService {
 
-    public List getAvailableBrands(LiquorType type){
-
-        List brands = new ArrayList();
-
+    public static List<String> getAvailableBrands(LiquorType type){
+        List<String> brands = new ArrayList<String>();        
         if(type.equals(LiquorType.HARYANA)){
             brands.add("Faridabad");
-            brands.add(("Gurugram"));
-
-        }/*else if(type.equals(LiquorType.TAMILNADU)){
-            brands.add("Chennai");
-            brands.add("Mahabalipuram");
-
-        }*/else if(type.equals(LiquorType.MAHARASHTRA)){
+        } else if(type.equals(LiquorType.ODISHA)){
+            brands.add("Bhubaneswar");     
+        } else if(type.equals(LiquorType.TAMILNADU)){
+        	brands.add("Chennai");  
+        	brands.add("Mahabalipuram");         
+        } else if(type.equals(LiquorType.MAHARASHTRA)){
             brands.add("Mumbai");
-
         }else {
             brands.add("No Information Available");
         }
-    return brands;
+        return brands;
     }
 }
